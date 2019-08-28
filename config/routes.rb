0054bad3f5c8f7985217c76bc464612858d31079
tbcsoft <fatihsahan1150@gmail.com>
2019-08-28 17:39:42 +0300

    done

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  root "posts#index"
+  devise_for :users
+  resources :posts do
+    member do
+      put "like", to: "posts#like"
+      put "unlike", to: "posts#unlike"
+    end
+    resources :comments
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
